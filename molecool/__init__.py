be245c097d86e8b38799accf23f390e3b4a3b5b3
Belloj3 <55210491+Belloj3@users.noreply.github.com>
2021-05-27 16:17:41 -0400

    restructuring package into moldules

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,7 +4,16 @@ A python package workshop, lead by molssi.
 """
 
 # Add imports here
-from .functions import *
+from .functions import canvas,zen
+from .measure import calculate_distance, calculate_angle
+from .molecule import build_bond_list
+from .visualize import draw_molecule, bond_histogram
+
+
+import molecool.IO 
+ 
+
+
 
 # Handle versioneer
 from ._version import get_versions
@@ -12,3 +21,5 @@ versions = get_versions()
 __version__ = versions['version']
 __git_revision__ = versions['full-revisionid']
 del get_versions, versions
+
+
